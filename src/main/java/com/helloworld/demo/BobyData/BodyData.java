6437d252107274aed7fd3f30ae33ee835c968f9b
nkdev2024 <167497378+nkdev2024@users.noreply.github.com>
2024-04-19 11:09:30 +0000

    add body data

---
+++ src/main/java/com/helloworld/demo/BobyData/BodyData.java
@@ -0,0 +1,15 @@
+package com.helloworld.demo.BobyData;
+
+public class BodyData {
+    private String firstName;
+    private Number rollNumber;
+// Getters for firstName and rollNumber
+public String getFirstName() {
+    return firstName;
+}
+
+public Number getRollNumber() {
+    return rollNumber;
+}
+
+}
--- src/main/java/com/helloworld/demo/DemoApplication.java
+++ src/main/java/com/helloworld/demo/DemoApplication.java
@@ -2,8 +2,14 @@ package com.helloworld.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.helloworld.demo.BobyData.BodyData;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -24,5 +30,22 @@ public class DemoApplication {
         public String greet() {
             return "Greetings from Spring Boot!";
         }
+         
+        @PostMapping("/create")
+    public String create(@RequestBody BodyData requestData) {
+        String firstName = requestData.getFirstName();
+        return "hResource created successfully! Data received: " + firstName;
+    }
+
+    @PutMapping("/update")
+    public String update() {
+        return "Resource updated successfully!";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete() {
+        return "Resource deleted successfully!";
+    }
+        
     }
 }
