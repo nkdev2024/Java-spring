d8f6ca22b5989da94f05f84e69929814fc03a62a
nkdev2024 <167497378+nkdev2024@users.noreply.github.com>
2024-04-19 07:17:07 +0000

    first commit

---
+++ src/main/java/com/helloworld/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.helloworld.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @RestController
+    public static class HelloWorldController {
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "Hello, World!";
+        }
+
+        @GetMapping("/greet")
+        public String greet() {
+            return "Greetings from Spring Boot!";
+        }
+    }
+}
