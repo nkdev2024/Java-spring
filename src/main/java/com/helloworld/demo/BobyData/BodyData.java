97e14995e144599f79ab8812a3f7f05cfdc66351
nkdev2024 <167497378+nkdev2024@users.noreply.github.com>
2024-04-19 11:19:27 +0000

    add user body data class

--- src/main/java/com/helloworld/demo/BobyData/BodyData.java
+++ src/main/java/com/helloworld/demo/BobyData/BodyData.java
@@ -2,11 +2,22 @@ package com.helloworld.demo.BobyData;
 
 public class BodyData {
     private String firstName;
+    private String lastName;
+    private String password;
     private Number rollNumber;
 // Getters for firstName and rollNumber
 public String getFirstName() {
     return firstName;
 }
+
+public String getLastName() {
+    return lastName;
+}
+
+public String getPassword() {
+    return password;
+}
+
 
 public Number getRollNumber() {
     return rollNumber;
